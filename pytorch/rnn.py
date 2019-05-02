716642102478e6e58ee6c9cf9cfb1794e0ce35c4
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2019-05-02 14:58:16 +0700

    add rnn

---
+++ pytorch/rnn.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn
+
+
+class RNNSequenceClassifier(nn.Module):
+    def __init__(self, hidden_size, nlayers, vocab_size, wdims, nclasses, dropout, wv=None, layer_outputs=False):
+        super().__init__()
+        self.embeds = nn.Embedding(vocab_size, wdims)
+        self.hidden_size = hidden_size
+        if wv is not None:
+            assert wv.shape == (vocab_size, wdims), "Incompatible word embedding shape"
+            self.embeds.load_state_dict({'weight': wv})
+        self.cells = nn.ModuleList([
+            nn.GRU(wdims, hidden_size, num_layers=1, bidirectional=True) for _ in range(nlayers)
+        ])
+        self.layer_outputs = layer_outputs
+        self.drops = nn.ModuleList([
+            nn.Dropout(dropout) for _ in range(nlayers)
+        ])
+        self.outs = nn.ModuleList([
+            nn.Linear(hidden_size*2, nclasses) for _ in range(nlayers)
+        ])
+
+    def init_hidden(self, batch_size):
+        return torch.zeros((2, batch_size, self.hidden_size))
+
+    def apply_mask(self, x, lens):
+        mask = torch.arange(0, x.size(1), 1)[None, :] >= lens[:, None]
+        mask = mask[..., None]
+        return x.masked_fill_(mask.to(x.device), 0.0)
+
+    def forward(self, inputs, lengths):
+        outputs = self.embeds(inputs)
+        outputs = nn.utils.rnn.pack_padded_sequence(outputs, lengths)
+        if self.layer_outputs:
+            all_outputs = []
+        for drop, cell, out in zip(self.drops, self.cells, self.outs):
+            pre_outputs, _ = cell(outputs, self.init_hidden(inputs.size(1)).to(inputs.device))
+            outputs = drop(pre_outputs) + outputs
+            if self.layer_outputs:
+                pad_outputs, pad_lengths = nn.utils.rnn.pad_packed_sequence(outputs)
+                pad_outputs = pad_outputs.permute(1, 0, 2)
+                pad_outputs = out(pad_outputs)
+                pad_outputs = self.apply_mask(pad_outputs, pad_lengths)
+                all_outputs.append(all_outputs)
+        if self.layer_outputs:
+            return all_outputs
+        return outputs
