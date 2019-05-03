2bc244d82659ccc7e7b4f8162bf069a048566c0d
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2019-05-04 04:38:49 +0700

    add init

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -13,6 +13,8 @@ def init_transformer(m):
             nn.init.normal_(m.weight, 1., 0.02)
         if hasattr(m, 'bias') and m.bias is not None:
             nn.init.constant_(m.bias, 0.)
+    elif classname.find('Parameter') != -1:
+        nn.init.normal_(m, 0., 0.02)
     elif classname.find('TransformerXL') != -1:
         if hasattr(m, 'u'):
             nn.init.normal_(m.u, 0., 0.02)
