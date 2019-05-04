3aebf29203b21c445ad07db877a93044a9f22e5d
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2019-05-04 12:16:04 +0700

    add sampler

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 from torch import nn
+from torch.utils.data import Sampler
 
 
 def init_transformer(m):
@@ -20,3 +22,20 @@ def init_transformer(m):
             nn.init.normal_(m.u, 0., 0.02)
         if hasattr(m, 'v'):
             nn.init.normal_(m.v, 0., 0.02)
+
+
+class BucketByLengthSampler(Sampler):
+    def __init__(self, data_source, buckets):
+        self.data_source = data_source
+        self.bucket_len = buckets + [1e9]
+        self.lens = np.array([len(x) for x in data_source], dtype=np.int32).sort()
+
+    def __iter__(self):
+        self.buckets = [((self.lens < max_len) & (self.lens >= min_len)).nonzero()[0] for min_len, max_len in zip(self.bucket_len[:-1], self.bucket_len[1:])]
+        np.random.shuffle(self.buckets)
+        for b in self.buckets:
+            np.random.shuffle(b)
+        return iter(np.concatenate(self.buckets))
+
+    def __len__(self):
+        return len(self.data_source)
