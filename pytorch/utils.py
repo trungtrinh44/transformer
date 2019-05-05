dfe867febc33f694cddba2a8b53d63bed4def7b3
Trịnh Quốc Trung <trinhtrung96@gmail.com>
2019-05-05 22:59:26 +0700

    new sampler

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from torch import nn
 from torch.utils.data import Sampler
 
@@ -25,17 +26,47 @@ def init_transformer(m):
 
 
 class BucketByLengthSampler(Sampler):
-    def __init__(self, data_source, buckets):
+    def __init__(self, data_source, buckets, batch_size):
         self.data_source = data_source
         self.bucket_len = [0] + buckets + [1e9]
         self.lens = np.array([len(x) for x in data_source], dtype=np.int32)
+        self.batch_size = batch_size
 
     def __iter__(self):
         self.buckets = [((self.lens < max_len) & (self.lens >= min_len)).nonzero()[0] for min_len, max_len in zip(self.bucket_len[:-1], self.bucket_len[1:])]
         np.random.shuffle(self.buckets)
         for b in self.buckets:
             np.random.shuffle(b)
-        return iter(np.concatenate(self.buckets))
+        items = np.concatenate(self.buckets)
+        nbatch = (len(self.data_source)-1)//self.batch_size + 1
+        if len(items) < nbatch * self.batch_size:
+            items = np.concatenate([self.bucket_len, np.zeros((nbatch*self.batch_size-len(items),), np.int32)-1])
+        items = np.reshape(items, (nbatch, self.batch_size))
+        np.random.shuffle(items)
+        items = items.flatten()
+        items = items[items >= 0]
+        for idx in range(0, len(items), self.batch_size):
+            indices = items[idx:idx+self.batch_size]
+            batch = [self.data_source[i] for i in indices]
+            yield batch
 
     def __len__(self):
         return len(self.data_source)
+
+
+class Padder(object):
+    def __init__(self, maxlen):
+        self.maxlen = maxlen
+
+    def padding(self, x):
+        seq_lens = np.array([len(y) for y in x], dtype=np.int64)
+        out = np.zeros((len(x), max(seq_lens)), dtype=np.int64)
+        for ind, y in zip(out, x):
+            ind[:len(y)] = y
+        return torch.from_numpy(out), torch.from_numpy(seq_lens)
+
+    def __call__(self, tensors):
+        maxlen = self.maxlen
+        seq_len = maxlen
+        texts = [x[:seq_len] for x in tensors]
+        return self.padding(texts)
